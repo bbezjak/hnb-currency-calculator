e2477a318eb9f28211f0d4ec64e2f7ea8e9c69ac
Borna <borna.bezjak@gmail.com>
2020-07-28 15:34:26 +0200

    initial commit, created scafoldings

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './components';
+import { CurrencyPage } from './pages/currency-page/CurrencyPage';
+
+function App() {
+  return (
+    <>
+      <CurrencyPage/>
+    </>
+  )
+}
+
+export default App;
